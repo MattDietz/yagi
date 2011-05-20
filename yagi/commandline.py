12a883de8da64e768d5aebdcb90230363edfeeb3
Matt Dietz <matt.dietz@rackspace.com>
2011-05-20 11:45:51 -0500

    Forgot to add commandline.py

---
+++ yagi/commandline.py
@@ -0,0 +1,9 @@
+import argparse
+
+args = None
+
+def parse_args(desc):
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('--config', '-c', help='The path to the config file')
+    args = parser.parse_args()
+    return args
