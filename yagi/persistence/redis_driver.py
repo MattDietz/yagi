4b2090e6eed10477090de2fa33f56037b76b67a8
Monsyne Dragon <mdragon@rackspace.com>
2011-06-24 21:22:40 +0000

    Changed redis storage schema to allow paging, and ordered retreival of events. Also fixed dup event bug in broker.

--- yagi/persistence/redis_driver.py
+++ yagi/persistence/redis_driver.py
@@ -21,25 +21,55 @@ class Driver(yagi.persistence.Driver):
         super(yagi.persistence.Driver, self).__init__()
 
     def create(self, key, entity_uuid, value):
-        self.client.hset(key, entity_uuid, json.dumps(value))
+        self.client.set('entry:%s:content' % entity_uuid, json.dumps(value))
+        self.client.set('entry:%s:event_type' % entity_uuid, key)
+        self.client.lpush('type:%s' % key, entity_uuid)
+        self.client.lpush('entries', entity_uuid)
 
     def get(self, key, entity_uuid):
         def generator():
-            element = self.client.hget(key, entity_uuid)
-            yield entity_uuid, json.loads(element)
-        return self._format(key, generator)
+            element = self.client.get('entry:%s:content' % entity_uuid)
+            yield entity_uuid, element, key
+        return self._format(generator)
 
-    def get_all(self):
-        keys = self.client.keys()
-        return [e for key in keys for e in self.get_all_of_type(key)]
-
-    def get_all_of_type(self, key):
+    def get_all(self, page_size=None, page=-1):
+        length = self.client.llen('entries')
+        start, end = self._page(page, page_size, length)
+        uuids = self.client.lrange('entries', start, end)
         def generator():
-            elements = self.client.hgetall(key)
-            for k, v in elements.iteritems():
-                yield k, v
-        return self._format(key, generator)
+            for uuid in  uuids:
+                event_type = self.client.get('entry:%s:event_type' % uuid)
+                content = self.client.get('entry:%s:content' % uuid)
+                yield uuid, content, event_type
+        return self._format(generator)
 
-    def _format(self, key, gen):
+    def get_all_of_type(self, key, page_size=None, page=-1):
+        length = self.client.llen('type:%s' % key)
+        start, end = self._page(page, page_size, length)
+        uuids = self.client.lrange('type:%s' % key, start, end)
+        def generator():
+            for uuid in  uuids:
+                yield uuid, self.client.get('entry:%s:content' % uuid), key
+        return self._format(generator)
+
+    def _page(self, page, pagesize, length):
+        """ It may seem odd to have paging logic in the persistance drivers
+        here, but this logic differs depending on the persistance store (i.e.
+        a relational db would have to calculate offset and limit args, which
+        are different than Redis's list indexes.) """
+        if not pagesize:
+            return (0, -1)
+        pages = (length // pagesize) + 1 if length % pagesize \
+                                         else length // pagesize
+        if page < 0:
+            page = pages - (page * -1)
+        if page < 0 or page >= pages:
+            raise IndexError("Invalid page")
+        end = (length - (page * pagesize)) - 1
+        start = (end - pagesize) + 1
+        start = start if start > 0 else 0
+        return (start, end)
+
+    def _format(self, gen):
         return [{'id': uuid, 'content': json.loads(element),
-                 'event_type': key} for uuid, element in gen()]
+                 'event_type': key} for uuid, element, key in gen()]
